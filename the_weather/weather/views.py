c401e919d4b9cd17f3ed07799c4f344e395c82b2
Aditya Kotwal <adityakotwal@live.com>
2020-11-24 01:16:47 +0300

    .

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
-    return render(request,'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=2a9da8f0ce5b25af4cebee6ea78af333'
+    city = 'Las Vegas'
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon ' : r['weather'][0]['icon'],
+
+    }
+
+    context = {'city_weather' : city_weather}
+    return render(request,'weather/weather.html', context)
